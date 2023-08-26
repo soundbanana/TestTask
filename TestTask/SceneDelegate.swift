df330d38d4387be95bb316abd1943860d948fbdb
Daniil Chemaev <chemaev.ds@gmail.com>
2023-08-26 15:00:31 +0300

    Initial commit

---
+++ TestTask/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  TestTask
+//
+//  Created by Daniil Chemaev on 26.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        self.window = window
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ TestTask/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Daniil Chemaev on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
